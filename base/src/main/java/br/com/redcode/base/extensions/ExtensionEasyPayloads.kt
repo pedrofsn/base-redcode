87eed9f9dd3c1fae622669c7d20391df3a2601ed
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2019-01-04 07:13:41 -0200

    Adding 'Long' in extract class

--- base/src/main/java/br/com/redcode/base/extensions/ExtensionEasyPayloads.kt
+++ base/src/main/java/br/com/redcode/base/extensions/ExtensionEasyPayloads.kt
@@ -20,5 +20,6 @@ object extract {
     infix fun safe(value: Boolean?): Boolean = value ?: false
     infix fun safe(value: Float?): Float = value ?: 0.0f
     infix fun safe(value: Double?): Double = value ?: 0.0
+    infix fun safe(value: Long?): Long = value ?: -1
 
 }

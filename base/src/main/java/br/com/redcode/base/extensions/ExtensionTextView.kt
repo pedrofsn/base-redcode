633cd85ed11879829917000733dac2f495ea5fa8
Suvid Sahay <42059321+suvidsahay@users.noreply.github.com>
2019-10-22 18:32:33 +0530

    Deleted Extra lines
    Issue #16

--- base/src/main/java/br/com/redcode/base/extensions/ExtensionTextView.kt
+++ base/src/main/java/br/com/redcode/base/extensions/ExtensionTextView.kt
@@ -43,7 +43,6 @@ fun TextView.handleDate(callback: ((String) -> Unit)? = null) {
         text = result
         callback?.let { result }
 
-        setTextColor(ContextCompat.getColor(context, android.R.color.black))
     }
 
     setOnClickListener { Alerts.showDatePicker(context, calendar, callbackBirthday) }

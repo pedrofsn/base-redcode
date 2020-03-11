b4779875a8d46a2271dde35dc7d7e120e107a33c
pedrofsn <pedrokra@gmail.com>
2020-03-11 17:24:07 -0300

    adding fragment mvvm typealias

--- mvvm-restful-databinding/src/main/java/br/com/redcode/base/mvvm/restful/databinding/domain/Alias.kt
+++ mvvm-restful-databinding/src/main/java/br/com/redcode/base/mvvm/restful/databinding/domain/Alias.kt
@@ -1,9 +1,11 @@
 package br.com.redcode.base.mvvm.restful.databinding.domain
 
 import br.com.redcode.base.mvvm.restful.databinding.impl.ActivityMVVMDataBinding
+import br.com.redcode.base.mvvm.restful.databinding.impl.FragmentMVVMDataBinding
 
 /*
     CREATED BY @PEDROFSN IN 11/03/20 15:51
 */
 
 typealias ActivityMVVM<B, VM> = ActivityMVVMDataBinding<B, VM>
+typealias FragmentMVVM<B, VM> = FragmentMVVMDataBinding<B, VM>

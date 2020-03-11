e3774047820d72a7b4fa058de77f9c588d3e08dc
pedrofsn <pedrokra@gmail.com>
2020-03-11 17:11:07 -0300

    fix returns

--- mvvm-databinding/src/main/java/br/com/redcode/base/mvvm/databinding/extensions/ExtensionDelegates.kt
+++ mvvm-databinding/src/main/java/br/com/redcode/base/mvvm/databinding/extensions/ExtensionDelegates.kt
@@ -3,14 +3,17 @@ package br.com.redcode.base.mvvm.databinding.extensions
 import androidx.databinding.BaseObservable
 import kotlin.properties.Delegates
 
-fun BaseObservable.watchBR(fieldId: Int) {
-    Delegates.observable("") { _, _, _ -> notifyPropertyChanged(fieldId) }
-}
+fun BaseObservable.watchBR(fieldId: Int) =
+    Delegates.observable("") { _, _, _ ->
+        notifyPropertyChanged(fieldId)
+    }
 
-fun <T> BaseObservable.watchBR(fieldId: Int, defaultValue: T) {
-    Delegates.observable(defaultValue) { _, _, _ -> notifyPropertyChanged(fieldId) }
-}
+fun <T> BaseObservable.watchBR(fieldId: Int, defaultValue: T) =
+    Delegates.observable(defaultValue) { _, _, _ ->
+        notifyPropertyChanged(fieldId)
+    }
 
-fun BaseObservable.watchBoolean(fieldId: Int, defaultValue: Boolean = false) {
-    Delegates.observable(defaultValue) { _, _, _ -> notifyPropertyChanged(fieldId) }
-}
+fun BaseObservable.watchBoolean(fieldId: Int, defaultValue: Boolean = false) =
+    Delegates.observable(defaultValue) { _, _, _ ->
+        notifyPropertyChanged(fieldId)
+    }
